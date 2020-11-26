0f97544dcbec4214e662d3a27299b2602535c9ba
kinga.tittl@capgemini.com <kinga.tittl@capgemini.com>
2020-11-26 13:19:38 +0100

    Add mappers between DB models and Rest responses

--- src/main/resources/archetype-resources/src/main/java/repositories/SampleRepository.java
+++ src/main/resources/archetype-resources/src/main/java/repositories/SampleRepository.java
@@ -4,6 +4,11 @@ import ${package}.models.Sample;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SampleRepository extends JpaRepository<Sample, Long> {
+
+	@Override
+	List<Sample> findAll();
 
 }
