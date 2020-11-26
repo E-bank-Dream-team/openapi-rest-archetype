b3aae268fc789795c87cd5575193bb44bece0870
gwlodawiec <gwlodawiec@gmail.com>
2020-11-26 10:46:51 +0100

    Add simple contract test for AppController

---
+++ src/main/resources/archetype-resources/src/test/java/api/RestBase.java
@@ -0,0 +1,25 @@
+package ${package}.api;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+
+import ${package}.controllers.AppController;
+
+@RunWith(MockitoJUnitRunner.class)
+public abstract class RestBase {
+	
+	@InjectMocks
+	AppController appController;
+	
+	@Before
+    public void setup() {
+		RestAssuredMockMvc.standaloneSetup(
+                MockMvcBuilders.standaloneSetup(appController));
+	}
+	
+}
